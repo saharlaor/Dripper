6eefe443332aa2ca390648d4cdecfee7671fe29e
saharoori <saharlaor@gmail.com>
2022-02-01 14:22:51 +0200

    Client - Homepage layout using antd

--- client/src/components/Screens/Home/Home.jsx
+++ client/src/components/Screens/Home/Home.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./Home.css";
+import { Layout } from "antd";
+
+const { Header, Footer, Content } = Layout;
 
 function Home() {
-  return <div className="Home">Home</div>;
+  return (
+    <div className="Home">
+      <Layout>
+        <Header>Header</Header>
+        <Content>Content</Content>
+        <Footer>Footer</Footer>
+      </Layout>
+    </div>
+  );
 }
 
 export default Home;
