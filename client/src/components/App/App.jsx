2367295cd0d6e56461eb74bea302280c1d2bf421
saharoori <saharlaor@gmail.com>
2022-02-01 13:50:00 +0200

    Client - Empty homepage

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "../Screens/Home/Home";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/Screens/Home/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return <div className="Home">Home</div>;
+}
+
+export default Home;
