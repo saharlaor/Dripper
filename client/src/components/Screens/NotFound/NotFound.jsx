67e690fccd1b7a724b928f925a052fc184d5307c
saharoori <saharlaor@gmail.com>
2022-02-01 15:02:07 +0200

    Client - 404 page design using antd

--- client/src/components/Screens/NotFound/NotFound.jsx
+++ client/src/components/Screens/NotFound/NotFound.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import { Result, Button } from "antd";
+import { useNavigate } from "react-router-dom";
+
 import "./NotFound.css";
 
 function NotFound() {
+  const navigator = useNavigate();
+
+  const handleBackHomeClick = () => {
+    navigator("/");
+  };
+
   return (
     <div className="NotFound">
-      <h2>404 Not Found</h2>
+      <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={
+          <Button type="primary" onClick={handleBackHomeClick}>
+            Back Home
+          </Button>
+        }
+      />
     </div>
   );
 }
