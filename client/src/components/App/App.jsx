cafd74daf5437423688ad6e919db264e78b35061
saharoori <saharlaor@gmail.com>
2022-02-01 13:50:50 +0200

    Client - 404 Fallback

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "../Screens/Home/Home";
+import NotFound from "../Screens/NotFound/NotFound";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,8 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
     </div>
---
+++ client/src/components/Screens/NotFound/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./NotFound.css";
+
+function NotFound() {
+  return (
+    <div className="NotFound">
+      <h2>404 Not Found</h2>
+    </div>
+  );
+}
+
+export default NotFound;
