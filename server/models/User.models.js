51c44899efd61108985577278347cb2b60a40f95
saharoori <saharlaor@gmail.com>
2022-02-04 22:48:58 +0200

    Updated user mongo schema

--- server/models/User.models.js
+++ server/models/User.models.js
@@ -1,14 +1,40 @@
 // Imports
 const { Schema, model } = require("mongoose");
+validator = require("validator");
 
 const UserSchema = Schema({
-  authId: {
-    type: Schema.Types.ObjectId,
+  uid: {
+    type: String,
     required: true,
+    unique: true,
   },
   name: {
     type: String,
     require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+    validate: {
+      validator: function (val) {
+        // Generated automatically by google auth right now
+        // Future proofing for manual logins
+        validator.isEmail(val);
+      },
+    },
+  },
+  photoUrl: {
+    type: String,
+    validate: {
+      validator: function (val) {
+        validator.isURL(val);
+      },
+    },
+  },
+  drinkHistory: {
+    type: [Schema.Types.ObjectId],
+    default: [],
   },
 });
 
